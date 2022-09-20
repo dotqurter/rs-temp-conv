b5eef1c3e226c0738c633492747809390f15cfe4
dotqurter <rigglemangamers@gmail.com>
2022-09-20 18:00:48 -0400

    had a better idea for determining the conversion factor

--- src/main.rs
+++ src/main.rs
@@ -8,53 +8,51 @@ enum Measure {
 fn main() {
     println!("Convert from:");
     let mut fromstr = String::new();
-    std::io::stdin().read_line(&mut fromstr).expect("Failed to read line");
+    std::io::stdin().read_line(&mut fromstr).expect("Failed to read_line");
 
     println!("Convert to:");
     let mut tostr = String::new();
-    std::io::stdin().read_line(&mut tostr).expect("Failed to read line");
+    std::io::stdin().read_line(&mut tostr).expect("Failed to read_line");
 
     let lconv = get_conversion(fromstr.clone());
     let rconv = get_conversion(tostr);
 
     let lval = get_numbers(fromstr.clone());
     let res: f32;
-    if lconv != rconv {
-        match lconv {
-            Measure::Fahrenheit => {
-                res = match rconv {
-                    Measure::Celsius => to_celsius(lval),
-                    Measure::Kelvin => to_kelvin( to_celsius(lval) ),
-                    _ => std::panic!(),
-                };
-            },
-            Measure::Celsius => {
-                res = match rconv {
-                    Measure::Fahrenheit => to_fahrenheit(lval),
-                    Measure::Kelvin => to_kelvin(lval),
-                    _ => std::panic!(),
-                };
-            },
-            Measure::Kelvin => {
-                res = match rconv {
-                    Measure::Celsius => lval - 273.15,
-                    Measure::Fahrenheit  => to_fahrenheit(lval - 273.15),
-                    _ => std::panic!(),
-                };
-            },
-        }
-    } else {
-        res = lval;
+    match lconv {
+        Measure::Fahrenheit => {
+            res = match rconv {
+                Measure::Celsius => to_celsius(lval),
+                Measure::Kelvin => to_kelvin( to_celsius(lval) ),
+                Measure::Fahrenheit => lval,
+            };
+        },
+        Measure::Celsius => {
+            res = match rconv {
+                Measure::Fahrenheit => to_fahrenheit(lval),
+                Measure::Kelvin => to_kelvin(lval),
+                Measure::Celsius => lval,
+            };
+        },
+        Measure::Kelvin => {
+            res = match rconv {
+                Measure::Celsius => lval - 273.15,
+                Measure::Fahrenheit  => to_fahrenheit(lval - 273.15),
+                Measure::Kelvin => lval,
+            };
+        },
     }
     println!("{} {:?}", res, rconv);
 }
  
 fn get_conversion(input: String) -> Measure {
-    return match input.to_lowercase() {
-        _w if _w.contains("f") => Measure::Fahrenheit,
-        _w if _w.contains("c") => Measure::Celsius,
-        _w if _w.contains("k") => Measure::Kelvin,
-        String {..} => std::unimplemented!(),
+    let mut _temp: String = input.to_lowercase().chars().filter( |&c| c == 'f' || c == 'c' || c == 'k').collect();
+    _temp.truncate(1);
+    return match _temp.as_str() {
+        "f" => Measure::Fahrenheit,
+        "c" => Measure::Celsius,
+        "k" => Measure::Kelvin,
+        _   => std::panic!("Please give a conversion factor!"),
     }
 }
 
